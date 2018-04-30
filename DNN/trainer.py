d7d7171742ef83ac195a8eafbc6728965367dffb
EgroegCai <36320388+EgroegCai@users.noreply.github.com>
2018-04-29 23:38:17 -0400

    update: cuda

--- DNN/trainer.py
+++ DNN/trainer.py
@@ -16,12 +16,15 @@ class Trainer():
             self.classifier.cuda()
 
     def train(self,train_loader,epochs):
+
         for epoch in range(epochs):
             running_loss = 0.0
             for i,data in enumerate(train_loader,0):
                 inputs,labels = data
 
                 inputs, labels = Variable(inputs), Variable(labels)
+                if self.use_cuda:
+                    inputs = inputs.cuda()
 
                 self.opt.zero_grad()
                 outputs = self.classifier(inputs)
@@ -30,7 +33,7 @@ class Trainer():
                 self.opt.step()
 
                 running_loss += loss.data[0]
-                if i % 2000 == 1999:
+                if i % self.print_every == 1999:
                     print('[%d, %5d] Train loss: %.3f' %
                         (epoch + 1, i + 1, running_loss / 2000))
                     running_loss = 0.0
